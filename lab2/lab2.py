0b1075cf02a92c091074390ca94274b4454acce4
Daniil <daniil_demichev@mail.ru>
2020-03-08 17:20:13 +0300

    lab2

--- lab2/lab2.py
+++ lab2/lab2.py
@@ -1,6 +1,6 @@
 # Метод правых прямоугольников
 def rectangle_right(number, lower, higher, n):
-    h = (higher-lower)/n
+    h = (higher - lower) / n
     s = 0
     x = lower + h
     for i in range(n):
@@ -39,7 +39,49 @@ def find_n(number, lower, higher, n, accuracy):
         i1 = rectangle_mid(number, lower, h, n)
         if abs(i1 - i0) <= accuracy:
             return n
-        i0=i1
+        i0 = i1
+    return -1
+
+
+def find_n_left(number, lower, higher, n, accuracy):
+    h = (higher - lower) / n
+    i0 = rectangle_mid(number, lower, h, n)
+    for i in range(12):
+        n *= 2
+        i1 = rectangle_left(number, lower, h, n)
+        if abs(i1 - i0) <= accuracy:
+            print("Значение методом левых прямоугольников:", i1)
+            print("Число разбиений для достижения заданной точности:", n)
+            return
+        i0 = i1
+    return -1
+
+
+def find_n_right(number, lower, higher, n, accuracy):
+    h = (higher - lower) / n
+    i0 = rectangle_mid(number, lower, h, n)
+    for i in range(12):
+        n *= 2
+        i1 = rectangle_right(number, lower, h, n)
+        if abs(i1 - i0) <= accuracy:
+            print("Значение методом правых прямоугольников:", i1)
+            print("Число разбиений для достижения заданной точности:", n)
+            return
+        i0 = i1
+    return -1
+
+
+def find_n_mid(number, lower, higher, n, accuracy):
+    h = (higher - lower) / n
+    i0 = rectangle_mid(number, lower, h, n)
+    for i in range(12):
+        n *= 2
+        i1 = rectangle_mid(number, lower, h, n)
+        if 1/3*abs(i1 - i0) <= accuracy:
+            print("Значение методом средних прямоугольников:", i1)
+            print("Число разбиений для достижения заданной точности:", n)
+            return
+        i0 = i1
     return -1
 
 
@@ -56,14 +98,15 @@ def f(number, x):
 
 def solve(number, lower, higher, accuracy):
     n = 4
-    new_n = find_n(number, lower, higher, n, accuracy)
-    if new_n == -1:
+    new_n_left = find_n_left(number, lower, higher, n, accuracy)
+    if new_n_left == -1:
         print("Заданная точность не достигнута")
-        return
-    print("Значение методом правых прямоугольников:", rectangle_right(number, lower, higher, new_n))
-    print("Значение методом левых прямоугольников:", rectangle_left(number, lower, higher, new_n))
-    print("Значение методом средних прямоугольников:", rectangle_mid(number, lower, higher, new_n))
-    print("Число разбиений для достижения заданной точности:", new_n)
+    new_n_right = find_n_right(number, lower, higher, n, accuracy)
+    if new_n_right == -1:
+        print("Заданная точность не достигнута")
+    new_n_mid = find_n_mid(number, lower, higher, n, accuracy)
+    if new_n_mid == -1:
+        print("Заданная точность не достигнута")
 
 
 functions = ['y=2x^3+3', 'y=3x^4-4x^3+2x^2', 'y=7x^2-5x+2', 'y=5x^5-9x^3+8x']
