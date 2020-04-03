9de86fd94e3d43d515ffac5df82e6df7bc97ff13
Daniil <daniil_demichev@mail.ru>
2020-04-03 11:22:58 +0300

    Логарифмическая функция

--- lab4/lab4.py
+++ lab4/lab4.py
@@ -1,21 +1,18 @@
 import matplotlib.pyplot as plt
 from math import *
-
+from lab1 import lab1
 x = [0.5, 1, 1.5, 2, 2.5, 3, 3.5, 4, 4.5, 5, 5.5]
 y = [2.46, 3.29, 4.23, 5.43, 6.98, 8.96, 11.51, 14.78, 18.95, 24.31, 31.25]
 # plt.plot(x,y)
 # plt.grid(True)
 # plt.show()
 
+# Линейная функция
 n = len(x)
 sx = sum(x)
 sy = sum(y)
-sxx = 0
-for elem in x:
-    sxx += elem ** 2
-sxy = 0
-for i in range(n):
-    sxy += x[i] * y[i]
+sxx = sum([elem ** 2 for elem in x])
+sxy = sum([y[i] * x[i] for i in range(n)])
 a = (sxy * n - sx * sy) / (sxx * n - sx * sx)
 b = (sxx * sy - sx * sxy) / (sxx * n - sx * sx)
 # print(a,b)
@@ -29,8 +26,8 @@ slin = 0
 for i in range(n):
     slin += (ylin[i] - y[i]) ** 2
 # print(slin)
-from lab1 import lab1
 
+# Квадратичная функция
 sxxx = sum([elem ** 3 for elem in x])
 sxxxx = sum([elem ** 4 for elem in x])
 sxxy = sum([y[i] * x[i] ** 2 for i in range(n)])
@@ -46,6 +43,7 @@ for i in range(n):
     spol += (ypol[i] - y[i]) ** 2
 # print(spol)
 
+# Экспоненциальная функция
 sxy = sum([log(y[i]) * x[i] for i in range(n)])
 sy = sum([log(y[i]) for i in range(n)])
 a = exp((sxy * n - sx * sy) / (sxx * n - sx * sx))
@@ -60,3 +58,22 @@ sexp = 0
 for i in range(n):
     sexp += (yexp[i] - y[i]) ** 2
 # print(sexp)
+
+# Логарифмическая функция
+
+sx = sum([log(elem) for elem in x])
+sy = sum(y)
+sxx = sum([(log(elem)) ** 2 for elem in x])
+sxy = sum([y[i] * log(x[i]) for i in range(n)])
+a = (sxy * n - sx * sy) / (sxx * n - sx * sx)
+b = (sxx * sy - sx * sxy) / (sxx * n - sx * sx)
+for i in range(n):
+    print(a*log(x[i])+b, a*log(x[i])+b - y[i])
+ylog = [a*log(x[i])+b for i in range(n)]
+plt.plot(x,ylog)
+plt.grid(True)
+plt.show()
+slog = 0
+for i in range(n):
+    slog += (ylog[i] - y[i]) ** 2
+print(slog)
